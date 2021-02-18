b46cf06b49a1c8f6f714f31c228e006ba2b0a92c
juan <juan_jg@hotmail.com>
2021-02-18 00:04:40 -0300

    examples mugol swv e semihosting

---
+++ MCU1/Nucleo-F207ZG/002HelloMuGoL_semihosting/Debug/Startup/startup_stm32f207zgtx.d
@@ -0,0 +1,1 @@
+Startup/startup_stm32f207zgtx.o: ../Startup/startup_stm32f207zgtx.s
---
+++ MCU1/Nucleo-F207ZG/002HelloMuGoL_semihosting/Debug/Startup/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Startup/startup_stm32f207zgtx.s 
+
+OBJS += \
+./Startup/startup_stm32f207zgtx.o 
+
+S_DEPS += \
+./Startup/startup_stm32f207zgtx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Startup/startup_stm32f207zgtx.o: ../Startup/startup_stm32f207zgtx.s
+	arm-none-eabi-gcc -mcpu=cortex-m3 -g3 -c -x assembler-with-cpp -MMD -MP -MF"Startup/startup_stm32f207zgtx.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
+
